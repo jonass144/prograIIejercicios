2ae1960625da1c18ef97c005acc1292ecf9aa035
jonass144 <412341@tecnicatura.frc.utn.edu.ar>
2024-09-28 16:12:35 -0300

    actividad 03

---
+++ Actividad02WebApiFacturacionActividad03/Controllers/ArticulosController.cs
@@ -0,0 +1,91 @@
+﻿using Actividad02WebApiFacturacion.datos;
+using Actividad02WebApiFacturacion.dominio;
+using Actividad02WebApiFacturacion.servicio;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Actividad02WebApiFacturacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticulosController : ControllerBase
+    {
+
+
+
+
+    private IArticulosManager service_;
+        public ArticulosController()
+        {
+            service_ = new ArticuloManager();
+        }
+       
+        [HttpGet("Articulos")]
+        public IActionResult Get()
+        {
+
+            return Ok(service_.GetArticulos());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Articulos articulo)
+        {
+            try
+            {
+                if (articulo == null)
+                {
+                    return BadRequest("se esperaba un articulo");
+                }
+                if (service_.SaveArticulo(articulo))
+                    return Ok("articulo guardado con exito");
+                else
+                    return StatusCode(500, "no se pudo guardar el articulo");
+                
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "error interno ");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                if(id == null)
+                {
+                    return BadRequest("no se encontro articulo");
+                }
+                if (service_.Delete(id))
+                    return Ok("articulo eliminado");
+                else return StatusCode(500, "eliminacion fallida vuelva a intentar");
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "error interno");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Articulos articulo)
+        {
+            try
+            {
+                if (articulo == null)
+                    return BadRequest("se esperaba un articulo");
+                if (service_.Update(articulo))
+                    return Ok("articulo actualizado con exito");
+                else
+                    return StatusCode(500, "no se actualizo el articulo");
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, "error interno");
+            }
+         
+
+        }
+    }
+}
---
+++ Actividad02WebApiFacturacionActividad03/Controllers/FacturasController.cs
@@ -0,0 +1,57 @@
+﻿
+using Actividad02WebApiFacturacion.dominio;
+using Actividad02WebApiFacturacion.servicio;
+using BiblioFacturacion.servicio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad02WebApiFacturacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturasController : Controller
+    {
+        private IFacturaManager _servicio;
+        public FacturasController()
+        {
+            _servicio = new FacturaManager();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_servicio.GetFacturas());
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "error interno");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Factura factura)
+        {
+            try
+            {
+                if(factura == null)
+                {
+                    return BadRequest("se esperaba una factura");
+                }
+                if (_servicio.SaveFactura(factura))
+                    return Ok("factura guardada con exito");
+                else
+                    return StatusCode(500,"la factura no se guardo, intetelo de nuevo");
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, "error interno");
+
+            }
+        }
+
+
+
+    }
+}
