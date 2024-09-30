cf071ffe2af0374eb11ac188d2ef8991552b15d1
jonass144 <412341@tecnicatura.frc.utn.edu.ar>
2024-09-30 16:14:40 -0300

    actividad 05

---
+++ Actividad05WebApiServicios/Repositories/IServiciosRepository.cs
@@ -0,0 +1,17 @@
+﻿using Actividad05WebApi.Models;
+
+namespace Actividad05WebApi.Repositories
+{
+    public interface IServiciosRepository
+    {
+        bool Save(TServicio servicio);
+
+        bool Delete(int id);
+
+        List<TServicio> GetAll();
+
+        bool Update(TServicio servicio, int id);
+
+        List<TServicio> GetAllFltros(string nombre);
+    }
+}
